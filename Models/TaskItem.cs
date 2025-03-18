cd44fc2df01ed7a9a8bb025704b0e47395959b1b
Levi Junior <139442686+juniorlevi11@users.noreply.github.com>
2025-03-18 13:49:20 +0000

    Adicionar modelo TaskItem com propriedades básicas para gerenciamento de tarefas

---
+++ Models/TaskItem.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorTarefas.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; } // Identificador único da tarefa
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } // Título da tarefa
+
+        [StringLength(500)]
+        public string Description { get; set; } // Descrição da tarefa
+
+        [Required]
+        public DateTime DueDate { get; set; } // Data de vencimento da tarefa
+
+        public bool IsCompleted { get; set; } = false; // Status da tarefa (completa ou não)
+    }
+}
